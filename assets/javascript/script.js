26588bd1217d1a46ac34cd2a9be9d1199f9445cd
Code with Suhaib <114051176+Naruto92devs@users.noreply.github.com>
2024-02-03 18:40:28 +0500

    Add files via upload

---
+++ assets/javascript/script.js
@@ -0,0 +1,8 @@
+// let body = document.querySelector(".body")
+// window.onscroll = (e)=> {
+//    if (window.pageYOffset > 500) {
+//       body.style.opacity= "0.2"
+//     } else{
+//       body.style.opacity= "1"      
+//     }
+//  }
